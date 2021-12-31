237ef50ea209ef617ce921e29d5d92b930389d79
Anwar Zaid <mnanwarmz@gmail.com>
2021-12-31 19:44:14 +0800

    inital commit

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+import '../css/app.css';
+import './bootstrap';
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from "@inertiajs/progress";
+import {ZiggyVue, Ziggy} from './router';
+
+InertiaProgress.init();
+
+createInertiaApp({
+    resolve: async (name) => {
+        const pages = import.meta.glob('../js/Pages/**/*.vue')
+
+        return (await pages[`../js/Pages/${name}.vue`]()).default;
+    },
+    setup({ el, app, props, plugin }) {
+        const vue = createApp({ render: () => h(app, props) })
+            .use(plugin)
+            .use(ZiggyVue, Ziggy);
+
+        vue.mount(el);
+    },
+});
